8b283bd26794ccf8897f47ae3ef541cab34bd3c2
nguyenthetinh <tinhnt0207@gmail.com>
2019-06-05 14:18:14 +0700

    PT bac 1

---
+++ EquationResolver.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class EquationResolver {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double a,b,c;
+        System.out.println("a :");
+        a =sc.nextDouble();
+        System.out.println("b :");
+        b =sc.nextDouble();
+        System.out.println("c :");
+        c =sc.nextDouble();
+        if (a != 0){
+            double answer = (c-b)/a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        }else {
+            if (b==0){
+                System.out.println("The solution is al x!");
+            }else {
+                System.out.println("No solution");
+            }
+        }
+    }
+}
